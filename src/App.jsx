7404ff875c940c1c1686eb41e1eedb1720df5613
anurag262000 <anuragmishra262000@gmail.com>
2024-01-25 13:11:52 +0530

    app.jsx path corection

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ import { getApiConfiguration, getGenres } from '../store/homeSlice';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Home from './pages/home/Home';
-import Details from './pages/details/details';
-import SearchResult from './pages/searchResult/searchResult';
-import Explore from './pages/explore/explore';
-import PageNotFound from './pages/404/pageNotFound';
+import Details from './pages/details/Details';
+import SearchResult from './pages/searchResult/SearchResult';
+import Explore from './pages/explore/Explore';
+import PageNotFound from './pages/404/PageNotFound';
 
 
 function App() {
