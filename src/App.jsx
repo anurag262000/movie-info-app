94f5be0575d465226e9cef4ccbf3bb3a9cfe9b17
anurag262000 <anuragmishra262000@gmail.com>
2024-01-25 13:38:20 +0530

    c

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import { fetchDataFromApi } from "./utils/api";
 import { useSelector, useDispatch } from "react-redux";
 import { getApiConfiguration, getGenres } from "../store/homeSlice";
 
-import Header from "./components/header/Header";
-import Footer from "./components/footer/Footer";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 import Home from "./pages/home/Home";
 import Details from "./pages/details/Details";
 import SearchResult from "./pages/searchResult/SearchResult";
